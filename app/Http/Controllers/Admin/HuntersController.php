a8f7c7e2c053100659589d70e4450bd462b6aaf7
Dev Deverson <drownedindev@gmail.com>
2021-11-06 20:59:01 -0500

    Questions in Comments (HuntersController.php)

--- app/Http/Controllers/Admin/HuntersController.php
+++ app/Http/Controllers/Admin/HuntersController.php
@@ -73,7 +73,10 @@ class HuntersController extends Controller
         if (!Auth::check()) {
             return redirect(route('login'));
         }
-
+//wtf is user()? It's a method of auth, how is it receiving
+//username from the database? And where the fuck do I find
+//all these goddamn methods? Also why are we not using $user =
+// Auth::user(); as in the documentation? What's the difference?
         $user = auth()->user();
 
         if (!$user->admin) {
@@ -87,6 +90,16 @@ class HuntersController extends Controller
      * @param CreateOrUpdateHunterRequest $request
      * @return Application|RedirectResponse|Redirector
      */
+    //I get that it's calling CreateOrUpdateHunterRequest, but
+    // what is $request? Is $request an object filled with the
+    // returned array? So calling CreateOrUpdate checks
+    // authorization with "authorize" then jumps down to
+    // "rules" and validates all this information then returns,
+    // for example, an array of key pairs name => Burt Gummer,
+    // src => what/ever/burtgummer.jpg etc and stuffs it into
+    // $request? Why do you need to use validated if it's
+    // already passed the request rules and been put into
+    // $request. Couldn't you just Hunter::create($request)?
     public function store(CreateOrUpdateHunterRequest $request)
     {
         $validated = $request->validated();
