abb545b125172b3d651fc314732c4e00a1a2f8b5
Dev Deverson <drownedindev@gmail.com>
2021-11-21 15:14:26 -0600

    Added strict types and docblock and fixed symlink

--- app/Http/Requests/CreateHuntersCommentRequest.php
+++ app/Http/Requests/CreateHuntersCommentRequest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
@@ -11,7 +13,7 @@ class CreateHuntersCommentRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'comment' => 'required',
